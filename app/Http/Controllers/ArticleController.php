8f181009fb29ef5f28cf3f12e6b9c8dea8b5ca6d
selaaulia <selaaulia1307@gmail.com>
2021-04-13 15:47:46 +0700

    Langkah 3 modifikasi kode pada fungsi update untuk proses mengubah image yang sebelumnya telah diupload

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -66,7 +66,7 @@ class ArticleController extends Controller
      */
     public function edit(Article $article)
     {
-        //
+        
     }
 
     /**
@@ -78,7 +78,19 @@ class ArticleController extends Controller
      */
     public function update(Request $request, Article $article)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if ($article->featured_image && file_exists(storage_path('app/public/' .$article->featured_image))){
+            \Storage::delete(['public/' . $article->featured_image]);
+        }
+        $image_name = $request->file('image')->store('images', 'public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return 'Artikel berhasil diedit';
     }
 
     /**
